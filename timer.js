4ce06baf136ffd359783aaaa047e8c9f9a1dbc88
CrypticHunt25 <chaushayesha191@gmail.com>
2025-02-08 12:28:19 +0530

    Update timer.js

--- timer.js
+++ timer.js
@@ -1,13 +1,20 @@
 // timer.js
 let startTime;
 
-window.onload = function () {
+function startTimer() {
     startTime = new Date().getTime();
     localStorage.setItem('startTime', startTime);
-};
+}
 
 function getElapsedTime() {
     const currentTime = new Date().getTime();
+    const startTime = localStorage.getItem('startTime');
     const elapsedTime = currentTime - startTime;
     return elapsedTime;
 }
+
+window.onload = function () {
+    if (!localStorage.getItem('startTime')) {
+        startTimer();
+    }
+};
